7e9b4aed7c90cf01dd330d2a99336805d4d4193f
miczi <miczipl@gmail.com>
2019-06-29 10:16:02 +0200

    test bug fix

--- games/tests.py
+++ games/tests.py
@@ -5,16 +5,16 @@ from .models import GameSession
 # Create your tests here.
 class GameSessionTestCase(TestCase):
     def setUp(self):
-        GameSession.create(game='tic_tac_toe')
-        GameSession.create(game='rock_paper_scissors')
+        GameSession.objects.create(game='tic_tac_toe')
+        GameSession.objects.create(game='rock_paper_scissors')
 
     def test_objects_have_different_ids(self):
-        tic_tac_toe = GameSession.get(game='tic_tac_toe')
-        rock_paper_scissors = GameSession.get(game='rock_paper_scissors')
+        tic_tac_toe = GameSession.objects.get(game='tic_tac_toe')
+        rock_paper_scissors = GameSession.objects.get(game='rock_paper_scissors')
         self.assertGreater(rock_paper_scissors.id, tic_tac_toe.id)
 
     def test_save_and_load_game_state(self):
-        tic_tac_toe = GameSession.get(game='tic_tac_toe')
+        tic_tac_toe = GameSession.objects.get(game='tic_tac_toe')
 
         state = {'x': 1,
                  'y': 2}
@@ -23,7 +23,7 @@ class GameSessionTestCase(TestCase):
         tic_tac_toe.save()
 
         del tic_tac_toe
-        tic_tac_toe = GameSession.get(game='tic_tac_toe')
+        tic_tac_toe = GameSession.objects.get(game='tic_tac_toe')
         self.assertEqual(tic_tac_toe.state, state)
 
         state = {'x': 3,
@@ -32,5 +32,5 @@ class GameSessionTestCase(TestCase):
         tic_tac_toe.save()
 
         del tic_tac_toe
-        tic_tac_toe = GameSession.get(game='tic_tac_toe')
+        tic_tac_toe = GameSession.objects.get(game='tic_tac_toe')
         self.assertEqual(tic_tac_toe.state, state)
